03d897083355561732543fd80003d78728c9d621
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-02 18:25:47 +0300

    fix2.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,60 @@
 package com.company;
-import com.sun.deploy.util.StringUtils;
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-class Main {
+class testing {
+    public static boolean regExp(String S){
+        Pattern pattern = Pattern.compile("^[a-zA-Z]*");
+        Matcher matcher = pattern.matcher(S);
+        return matcher.matches();
+    }
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
-        int k = in.nextInt();
-        String A = S.substring(0,k);
-        String B = S.substring(S.length()-k,S.length());
-        if(S.length()>=2)
-            System.out.println(B);
-            System.out.println(A);
+        if (regExp(S) == true)
+        {
+            int k = in.nextInt();
+
+            ArrayList<String> listMax = new ArrayList<>();
+            ArrayList<String> listMin = new ArrayList<>();
+            String max = null;
+            String min = null;
+            if (S.length() >= 2 && k > 0 && k < S.length() - k && k < S.length()/2 && S.length()<1000) {
+                for (int i = 0; i < S.length() - k; i++) {
+
+                    String firstSub = S.substring(i, i + k);
+                    String secondSub = S.substring(i + 1, k + i + 1);
+                    if (firstSub.compareTo(secondSub) > 0) {
+                        max = firstSub;
+                        min = secondSub;
+
+                    } else if (firstSub.compareTo(secondSub) < 0) {
+                        min = firstSub;
+                        max = secondSub;
+                    } else if (firstSub.compareTo(secondSub) == 0) {
+                        min = firstSub;
+                        max = secondSub;
+                    }
+                    listMax.add(i, max);
+                    listMin.add(i, min);
+                }
+                Collections.sort(listMin);
+                System.out.println(listMin.get(0));
+
+                Collections.sort(listMax);
+                System.out.println(listMax.get(listMax.size() - 1));
+            }
+
+           else System.out.print(S+"\n"+S+"\n");
+
+        }
     }
+
 }
