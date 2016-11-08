acb615900ad2567b218399aa6532cb60e3295117
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-08 18:19:40 +0300

    new2.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class testing {
-    public static boolean regExp(String S){
+    public static boolean regExp(String S) {
         Pattern pattern = Pattern.compile("^[a-zA-Z]*");
         Matcher matcher = pattern.matcher(S);
         return matcher.matches();
@@ -18,50 +18,33 @@ class testing {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
-        if (regExp(S) == true)
-        {
+        if (regExp(S) == true) {
             int k = in.nextInt();
 
             String max = "";
             String min = "";
 
-            if (S.length() >= 2 && k > 0 && k < S.length() - k && k < S.length()/2 && S.length()<1000) {
-                for (int i = 0; i < S.length() - k; i++) {
+            if (S.length() >= 2 && k > 0 && k < S.length() - k && k < S.length() / 2 && S.length() < 1000) {
+                for (int i = 0; i < S.length() - k+1; i++) {
 
                     String firstSub = S.substring(i, i + k);
-                    String secondSub = S.substring(i + 1, k + i + 1);
 
-                    String tempMax;
-                    String tempMin;
-
-                    if (firstSub.compareTo(secondSub) > 0) {
-                        tempMax = firstSub;
-                        tempMin = secondSub;
-                        if (i==0) {
-                            min = secondSub;
-                            max = firstSub;
-                        }
-                    } else {
-                        tempMin = firstSub;
-                        tempMax = secondSub;
-                        if (i==0) {
-                            min = firstSub;
-                            max = secondSub;
-                        }
+                    if (max.equals("") || min.equals("") ) {
+                        max = firstSub;
+                        min = firstSub;
                     }
 
-                    if(tempMax.compareTo(max) > 0)
-                        max = tempMax;
-                    if(tempMin.compareTo(min) < 0)
-                        min = tempMin;
-                }
-
+                        if (firstSub.compareTo(max) > 0) {
+                            max = firstSub;
+                        }
+                        if (firstSub.compareTo(min) < 0){
+                            min = firstSub;
+                        }
+                    }
                 System.out.println(min);
                 System.out.println(max);
             }
-           else System.out.print(S+"\n"+S+"\n");
-
+            else System.out.print(S+"\n"+S+"\n");
         }
     }
-
 }
